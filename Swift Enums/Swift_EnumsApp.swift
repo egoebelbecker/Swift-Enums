e8d599515fd6929546ea27194376407512b0109a
Eric Goebelbecker <eric@ericgoebelbecker.com>
2021-11-01 13:28:28 -0400

    Initial Commit

---
+++ Swift Enums/Swift_EnumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_EnumsApp.swift
+//  Swift Enums
+//
+//  Created by Eric Goebelbecker on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_EnumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
